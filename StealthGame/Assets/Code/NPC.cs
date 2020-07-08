e90f20dff8bb546589969ff10a8c0e69507c4c7f
Dylan Anderson <gymnaskid@gmail.com>
2020-07-08 16:34:00 -0600

    created a finiteStateMachine and attacjed to the NPC which currently will only idle -Dylan Anderson

---
+++ StealthGame/Assets/Code/NPC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent),typeof(FiniteStateMachine))]
+public class NPC : MonoBehaviour
+{
+    NavMeshAgent _navMeshAgent;
+    FiniteStateMachine _finiteStateMachine;
+    public void Awake()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+        _finiteStateMachine = this.GetComponent<FiniteStateMachine>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
