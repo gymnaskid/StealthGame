f8836a6f07b84aac8299c567c9f5bfde100f17aa
Dylan Anderson <gymnaskid@gmail.com>
2020-07-08 16:13:05 -0600

    created an IdelState to be used by the FSM it is currently empty - Dylan Anderson

---
+++ StealthGame/Assets/Code/FMS/States/IdleState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "IdleState", menuName = "Unity-FSM/States/Idle", order = 1)]
+public class IdleState : AbstractFSMState
+{
+    public override bool EnterState()
+    {
+        base.EnterState();
+        
+        Debug.Log("Entered idle state");
+        return true;
+    }
+    public override void UpdateState()
+    {
+        Debug.Log("Updateing idle state");
+    }
+
+    public override bool ExitState()
+    {
+        base.ExitState();
+        
+        Debug.Log("Exiting idle state");
+        return true;
+    }
+}
