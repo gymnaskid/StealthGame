d18c4e15054bdfb2524f72e8c25e43376c15bfbc
Dylan Anderson <gymnaskid@gmail.com>
2020-07-08 15:29:14 -0600

    added a connected waypoint script that will find nearby waypoints and allow the navmech agent to move to the next aypoint based on nearby waypoints. also added a NPCConnectedPatrol that will have the NPC patrol based on the COnnectedWaypoints - Dylan Anderson

---
+++ StealthGame/Assets/Code/ConnectedWaypoint.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ConnectedWaypoint : Waypoint
+{
+
+    [SerializeField]
+    protected float _connectivityRadius = 50f;
+
+    List<ConnectedWaypoint> _connections;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        //Make sure there are waypoints and they are tagged to create the list
+        if(allWaypoints.Length == 0)
+        {
+            Debug.LogError("There are no objects with tag of Waypoint");
+        }
+
+        _connections = new List<ConnectedWaypoint>();
+
+        //loop throught the waypoint list
+        for(int i = 0; i < allWaypoints.Length; i++)
+        {
+            //Make sure that the object tagged waypoint actiually has a Connectedwaypoint object attached to it
+            ConnectedWaypoint nextWaypoint = allWaypoints[i].GetComponent<ConnectedWaypoint>();
+
+            if(nextWaypoint != null)
+            {
+                //if the two objects are close enough add to _connections List
+                if(nextWaypoint != this && Vector3.Distance(this.transform.position, nextWaypoint.transform.position) <= _connectivityRadius)
+                {
+                    _connections.Add(nextWaypoint);
+                }
+            }
+        }
+    }
+
+    public override void OnDrawGizmos()
+    {
+        //Draw the base waypoint 
+        base.OnDrawGizmos();
+
+        //draw the connectivity radius
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _connectivityRadius);
+    }
+
+    public ConnectedWaypoint NextWaypoint(ConnectedWaypoint previousWaypoint)
+    {
+        //make sure there are connections
+        if(_connections.Count == 0)
+        {
+            //if there are no connections complain
+            Debug.LogError("Insufficient waypoints");
+            return null;
+        }
+        else if(_connections.Count == 1 && _connections.Contains(previousWaypoint))
+        {
+            //if there is only one connection and it is the previous one go back
+            return previousWaypoint;
+        }
+        else
+        {
+            //randomly pick a waypoint within range
+            ConnectedWaypoint nextWaypoint;
+            int nextIndex;
+
+            do
+            {
+                nextIndex = UnityEngine.Random.Range(0, _connections.Count);
+                nextWaypoint = _connections[nextIndex];
+            } while (nextWaypoint == previousWaypoint);
+
+            return nextWaypoint;
+        }
+    }
+}
---
+++ StealthGame/Assets/Code/NPCConnectedPatrol.cs
@@ -0,0 +1,115 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCConnectedPatrol : MonoBehaviour
+{
+    [SerializeField]
+    bool _partolWaiting;
+
+    [SerializeField]
+    float _totalWaitTime = 3f;
+
+    [SerializeField]
+    float _switchProbability = 0f;
+
+    NavMeshAgent _navMeshAgent;
+    ConnectedWaypoint _currentWaypoint;
+    ConnectedWaypoint _previousWaypoint;
+
+    bool _travelling;
+    bool _waiting;
+    float _waitTimer;
+    int _waypointsVisited;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("Ther is no nav mesh agent attatched to " + gameObject.name);
+        }
+        else
+        {
+            GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+            if (allWaypoints.Length > 0)
+            {
+                while (_currentWaypoint == null)
+                {
+                    int random = UnityEngine.Random.Range(0, allWaypoints.Length);
+                    ConnectedWaypoint startingWaypoint = allWaypoints[random].GetComponent<ConnectedWaypoint>();
+
+                    if (startingWaypoint != null)
+                    {
+                        _currentWaypoint = startingWaypoint;
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError("Failed to Find Waypoints");
+            }
+        }
+        SetDestination();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check to see if we are close to our destination
+        if (_travelling && _navMeshAgent.remainingDistance <= 1f)
+        {
+            _travelling = false;
+            _waypointsVisited++;
+
+            //If we are going to wait that wait
+            if (_partolWaiting)
+            {
+                _waiting = true;
+                _waitTimer = 0f;
+
+            }
+            else
+            {
+                //Set the next Destination
+                SetDestination();
+            }
+        }
+
+        //If we are waiting
+        if (_waiting)
+        {
+            _waitTimer += Time.deltaTime;
+            if(_waitTimer >= _totalWaitTime)
+            {
+                _waiting = false;
+                SetDestination();
+            }
+        }
+    } 
+    
+    
+    
+    private void SetDestination()
+    {
+        if(_waypointsVisited > 0)
+        {
+            ConnectedWaypoint nextWaypoint = _currentWaypoint.NextWaypoint(_previousWaypoint);
+            _previousWaypoint = _currentWaypoint;
+            _currentWaypoint = nextWaypoint;
+        }
+
+        Vector3 targetVector = _currentWaypoint.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+        _travelling = true;
+    }
+}
